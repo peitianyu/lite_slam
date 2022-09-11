89671323c8e1fbfa6c26ecb131edb0fb08c59dfc
武装带你 <1216582461@qq.com>
2022-09-11 18:42:14 +0800

    Update ndt_match.cc

--- src/scan_match/ndt/ndt_match.cc
+++ src/scan_match/ndt/ndt_match.cc
@@ -4,7 +4,6 @@
 NdtMatch::NdtMatch(const Options &options)
 :m_options(options)
 {
-    m_grids.resize(options.col_num * options.row_num + 1);
 }
 
 bool NdtMatch::Match( const std::vector<Eigen::Vector2f> &first_scan, const std::vector<Eigen::Vector2f> &second_scan, Eigen::Vector3f &robot_pose)
@@ -12,53 +11,55 @@ bool NdtMatch::Match( const std::vector<Eigen::Vector2f> &first_scan, const std:
     if( first_scan.empty() || second_scan.empty() )
         {return false;}
 
-    CalculateNdtGrid( first_scan );
-    
+    std::vector<GridCell> grids;
+    grids.resize(m_options.col_num * m_options.row_num + 1);
+
+    CalculateNdtGrid(first_scan, grids);
+
     Eigen::Vector3f pose_delta( 0.0f, 0.0f, 0.0f );
     for(uint iteration = 0; iteration < m_options.max_iterations; iteration ++ )     
-        {EstimateTransformationOnce( second_scan, pose_delta );}
-    
+    {
+        EstimateTransformationOnce(second_scan, grids, pose_delta);
+    }
+
     NormalizePose( pose_delta );
 
     if( fabs(pose_delta[0]) < 1 && fabs(pose_delta[1]) < 1 ){
         robot_pose += pose_delta;
     }
     return true;
-}   
-
-void NdtMatch::CalculateNdtGrid( const std::vector<Eigen::Vector2f> &scan )
-{
-    memset( m_grids.data(), 0, m_grids.size() * sizeof( m_grids[0] ) );     
-
-    for( Eigen::Vector2f point: scan){
-        int index = PointToGrid( point );   
-        m_grids[index].mean += point;
-        m_grids[index].points.push_back( point );
-    }   
-
-    for( size_t i = 0; i < m_grids.size(); i ++ ){
-        if( m_grids[i].points.size() >= 3 ){
-            m_grids[i].mean = m_grids[i].mean / static_cast<float>( m_grids[i].points.size() );
-
-            for( Eigen::Vector2f point : m_grids[i].points )
-                {m_grids[i].covarince += ( point - m_grids[i].mean ) * ( point - m_grids[i].mean ).transpose();}
-            m_grids[i].covarince /= static_cast<float>( m_grids[i].points.size()  - 1 );
-        }
-        else {
-            m_grids[i].covarince << std::numeric_limits<float>::max(), 0, 0, std::numeric_limits<float>::max();
-        }
-    }
-    
 }
 
-void NdtMatch::GetHessianDerived( const std::vector<Eigen::Vector2f> &scan, const Eigen::Vector3f &p, Eigen::Matrix3f &H, Eigen::Vector3f &b )
+void NdtMatch::CalculateNdtGrid(const std::vector<Eigen::Vector2f> &scan ,std::vector<GridCell> &grids)
+{
+    for( Eigen::Vector2f point: scan){
+        int index = PointToGrid( point );   
+        grids[index].mean += point;
+        grids[index].points.push_back( point );
+    }   
+
+    for( size_t i = 0; i < grids.size(); i ++ ){
+        if( grids[i].points.size() >= 3 ){
+            grids[i].mean = grids[i].mean / static_cast<float>( grids[i].points.size() );
+
+            for( Eigen::Vector2f point : grids[i].points )
+                {grids[i].covarince += ( point - grids[i].mean ) * ( point - grids[i].mean ).transpose();}
+            grids[i].covarince /= static_cast<float>( grids[i].points.size()  - 1 );
+        }
+        else {
+            grids[i].covarince << std::numeric_limits<float>::max(), 0, 0, std::numeric_limits<float>::max();
+        }
+    }
+}
+
+void NdtMatch::GetHessianDerived(const std::vector<Eigen::Vector2f> &scan, const Eigen::Vector3f &p, const std::vector<GridCell> &grids, Eigen::Matrix3f &H, Eigen::Vector3f &b)
 {
     for( Eigen::Vector2f point: scan ){
         Eigen::Vector2f point_in_first_frame = PointTransform( point, p );                
         int index = PointToGrid( point_in_first_frame );
 
-        Eigen::Vector2f error = point_in_first_frame - m_grids[index].mean;
-        Eigen::Matrix2f sigma_inverse = ( m_grids[index].covarince ).inverse(); 
+        Eigen::Vector2f error = point_in_first_frame - grids[index].mean;
+        Eigen::Matrix2f sigma_inverse = ( grids[index].covarince ).inverse(); 
 
         Eigen::Matrix<float, 2, 3> jacobian;
         jacobian << 1, 0,  -point[0] * ::sin( p[2] ) - point[1] * ::cos( p[2] ),
@@ -67,14 +68,14 @@ void NdtMatch::GetHessianDerived( const std::vector<Eigen::Vector2f> &scan, cons
         b += ( error.transpose() * sigma_inverse * jacobian ).transpose();
         H += jacobian.transpose() * sigma_inverse * jacobian;           
     }
-}   
+}
 
-void NdtMatch::EstimateTransformationOnce( const std::vector<Eigen::Vector2f> &scan, Eigen::Vector3f &p )            
+void NdtMatch::EstimateTransformationOnce(const std::vector<Eigen::Vector2f> &scan, const std::vector<GridCell> &grids, Eigen::Vector3f &p)
 {
     Eigen::Matrix3f H = Eigen::Matrix3f::Zero();
     Eigen::Vector3f b = Eigen::Vector3f::Zero();
-    GetHessianDerived( scan, p, H, b );
-    
+    GetHessianDerived(scan, p, grids, H, b);
+
     p -= H.inverse() * b; 
 }
    
